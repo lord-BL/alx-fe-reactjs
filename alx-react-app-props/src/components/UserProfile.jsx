5aacf136da480acd794700de42cb1a8afbc62f9a
Lord Lartey <lordlart24@gmail.com>
2025-02-22 16:55:22 +0000

    add usercontext

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,15 @@
-const UserProfile = (props) => {
+import { useContext } from "react";
+import UserContext from "../UserContext"; // Adjust the path if needed
+
+function UserProfile() {
+  const userData = useContext(UserContext); // Accessing userData from context
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
-};
+}
+
 export default UserProfile;
