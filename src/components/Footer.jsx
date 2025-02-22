a0bccedbe8d02c1cfe666501bd3a3f80876e1a76
Lord Lartey <lordlart24@gmail.com>
2025-02-22 10:17:32 +0000

    commit changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <p style={{ backgroundColor: "purple", color: "black" }}>
+          © 2023 City Lovers
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
