807ffb7300c672ce15209458a742acf8b37f3981
Lord Lartey <lordlart24@gmail.com>
2025-02-23 19:18:30 +0000

    h

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,15 +1,14 @@
-import "./App.css";
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import About from "./components/About";
-import Services from "./components/Services";
-import Contact from "./components/Contact";
+import Navbar from "./Components/Navbar";
+import Contact from "./Components/Contact";
+import Services from "./Components/Services";
+import About from "./Components/About";
+import Home from "./Components/Home";
+
 function App() {
   return (
     <Router>
-      <Navbar /> {/* Navbar appears on all pages */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -19,5 +18,4 @@ function App() {
     </Router>
   );
 }
-
 export default App;
