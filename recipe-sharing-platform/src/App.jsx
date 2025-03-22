28760ebb7180d515fd7ad5fa01d335ce38b3a82f
Lord Lartey <lordlart24@gmail.com>
2025-03-22 09:01:17 +0000

    Add recipe Sharing platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="grid grid-cols-2 h-96">
+        <div className="bg-slate-600 text-blue-500">Hey</div>
+        <div className="bg-slate-800">hey again</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
